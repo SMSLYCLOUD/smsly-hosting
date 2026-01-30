8f9b1a850ccde559ba17e3b68d537c45233b9d10
SMSLY CLOUD <smsly2025@gmail.com>
2026-01-30 19:34:42 +0100

    Add beautiful custom allauth templates with dark theme

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -138,7 +138,7 @@ SOCIALACCOUNT_PROVIDERS = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
